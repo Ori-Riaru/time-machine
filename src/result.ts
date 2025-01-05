2fb2aa5a82148388270ee9f96bf9dbfebff17ad9
RiaruAzaki <riaruazaki@proton.me>
2025-01-04 23:19:46 -0500

    Moved html files to /src

--- src/result.ts
+++ src/result.ts
@@ -1,11 +1,11 @@
 document.getElementById('return')?.addEventListener('click', () => {
-    window.location.href = 'index.html';
+    window.location.href = 'control.html';
 });
 
 window.onload = () => {
     let event = JSON.parse(localStorage.getItem('event')!);
 
-    document.getElementById('image')!.style.background = `url(../src-tauri/wallpapers/${event.path})`;
+    document.getElementById('image')!.style.background = `)`;
     document.getElementById('eventName')!.innerHTML = event.event;
     document.getElementById('description')!.innerHTML = event.description;
     document.getElementById('time')!.innerHTML = event.time.day + "/" + event.time.month + "/" + event.time.year;
